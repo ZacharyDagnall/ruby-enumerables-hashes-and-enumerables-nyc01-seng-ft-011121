e9d02f0333c628592d431ad1656b1bba20528004
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-11 14:17:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |k,v|
-    puts "Happy Birthday #{k}! You are now #{v} years old!"
+  birthday_kids.each do |name,age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
